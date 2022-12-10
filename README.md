55a5fd51fab4c5a7af0b85c5bc1dcf6a2dee278b
Aeonss <30298077+Aeonss@users.noreply.github.com>
2022-12-10 03:23:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+<p align="center"><img src="https://i.imgur.com/QwItpe6.png" width="200" height="200"/></p>
+
+<h1 align="center">BubbleBlaster</h1>
+
+<font size="3"><a href="https://github.com/Aeonss/BubbleBlaster/releases/latest/">Bubble Blaster</a> is a python script that remove text from speech bubbles in mangas/manhwas with OCR.
+
+<br>
+<p align="center">
+<a href="https://github.com/Aeonss/BubbleBlaster/releases/latest/"><img src="https://img.shields.io/github/v/release/Aeonss/BubbleBlaster?style=for-the-badge&label=%20%F0%9F%93%A3%20Latest%20release&color=778beb&labelColor=2f3542"/></a>
+<img src="https://img.shields.io/github/stars/Aeonss/BubbleBlaster?style=for-the-badge&label=%E2%AD%90%20Stars&color=786fa6&labelColor=2f3542"/>
+<img src="https://img.shields.io/github/downloads/Aeonss/BubbleBlaster/total.svg?style=for-the-badge&label=%E2%AC%87%EF%B8%8FDownloads&color=4b6584&labelColor=2f3542"/>
+</p>
+
+<font size="3">
+
+## 🔨 &nbsp; Installation
+Install python:
+``` bash
+https://www.python.org/downloads/
+```
+
+Download the latest development release:
+``` bash
+pip install git+https://github.com/Aeonss/BubbleBlaster.git
+```
+
+Download the requirements:
+``` bash
+pip install -r requirements.txt
+```
+
+
+## ❤️ &nbsp; Examples
+![example1](https://i.imgur.com/cyPm2cE.png)
+![example2](https://i.imgur.com/TIS5yIo.png)
+![example3](https://i.imgur.com/ivr6qv9.png)
+![example4](https://i.imgur.com/pSdQp5V.png)
+![example5](https://i.imgur.com/Fl3eCAm.png)
+![example6](https://i.imgur.com/QrCLCAO.png)
+
+
+## 🚀 &nbsp; Usage
+Example usage:
+``` bash
+python bubbleblaster.py -i PATH/IMAGE.png
+```
+
+Remove Korean text:
+``` bash
+python bubbleblaster.py -l ko -i image.png
+```
+
+Remove Korean text and translate text to English:
+``` bash
+python bubbleblaster.py -l ko -i image.png --t
+```
+
+Remove Korean text and console logs:
+``` bash
+python bubbleblaster.py -l ko -i image.png --d
+```
+
+Remove Japanese text:
+``` bash
+python bubbleblaster.py -l ja -i image.png
+```
+
+Remove Japanese text with confidence levels above 0.5:
+``` bash
+python bubbleblaster.py -l ja -i image.png -c 0.5
+```
+
+
+Arguments:
+``` bash
+-i  <image>         ->  Path to image [Required]
+-l  (language code) ->  (Default: English) -> Reference from: https://www.jaided.ai/easyocr/ [Optional]
+--c (confidence)    ->  (Default: 0.15)    -> Any text below this number will not be removed [Optional]
+```
+
+Flags:
+``` bash
+--t     -> Any foreign text will be replaced with English text (Default: false)
+--d     -> Show rectangle text and console message (Default: false)
+```
+
+
+
+## ✅ &nbsp; Additional Information
+* BubbleBlaster was created with [**EasyOCR**](https://github.com/JaidedAI/EasyOCR) and [**Deep-Translator**](https://github.com/nidhaloff/deep-translator).
+* Note that the machine translation is very bad, and the text placing is not optimized.
+* Please request any features or report any bugs in [issues](https://github.com/Aeonss/BubbleBlaster/issues).
+
+
+## 🤖 &nbsp; To Do
+* Make integrated translations better
+
+
+## 📘 &nbsp; License
+BubbleBlaster is released under the [MIT license](https://github.com/Aeonss/BubbleBlaster/blob/master/LICENSE.md).
+
+</font>
---
+++ bubbleblaster.py
@@ -0,0 +1,127 @@
+#----------------------------------------------------------------------------------------------------#
+
+#   References:
+#   https://github.com/JaidedAI/EasyOCR
+#   https://www.analyticsvidhya.com/blog/2021/06/text-detection-from-images-using-easyocr-hands-on-guide/
+#   https://stackoverflow.com/questions/39316447/opencv-giving-wrong-color-to-colored-images-on-loading
+
+#----------------------------------------------------------------------------------------------------#
+
+import sys, os.path, argparse
+import easyocr
+from deep_translator import GoogleTranslator
+import cv2
+from matplotlib import pyplot as plt
+
+#----------------------------------------------------------------------------------------------------#
+
+def OCR():
+    
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "-image", help="Image path", required=True)
+    parser.add_argument("-l", "-lang", help="Language code (https://www.jaided.ai/easyocr/)", nargs='?', const="en", default="en", required=False) # Default = en
+    parser.add_argument("-c", "-conf", help="Confidence value", nargs='?', const=0.15, default=0.15, type=float) # Default = 0.15
+    parser.add_argument("--t", "--translate", action='store_true', help="Replace foreign text with english text") # Default = False
+    parser.add_argument("--d", "--debug", action='store_true', help="Show rectangles around text") # Default = False
+        
+    args = parser.parse_args()
+       
+    IMAGE = args.i
+    LANG = args.l
+    TRANSLATE = args.t
+    CONFIDENCE = args.c
+    DEBUG = args.d
+    
+    if not os.path.exists(IMAGE) or (not os.path.splitext(IMAGE)[-1].lower() == ".png" and not os.path.splitext(IMAGE)[-1].lower() == ".jpg"):
+        print("ERROR: File does not exist or File is not a PNG or JPG!")
+        return
+
+    
+    
+    # OCR
+    reader = easyocr.Reader([LANG])
+    result = reader.readtext(IMAGE)
+
+
+    # Images
+    img_rect = cv2.imread(IMAGE)
+    img_temp = cv2.imread(IMAGE)
+    h, w, c = img_temp.shape
+
+
+    # Fill temp image with black
+    img_temp = cv2.rectangle(img_temp, [0,0], [w, h], (0, 0, 0), -1)
+    img_inpaint = cv2.imread(IMAGE)
+    
+    trans_list = []
+
+    # For each detected text
+    for r in result:
+        
+        # If the OCR text is above the CONFIDENCE
+        if r[2] >= CONFIDENCE:
+            
+            # Save the tuple of top left and bottom right of where the text is
+            top_left = tuple(int(x) for x in tuple(r[0][0]))
+            bottom_right = tuple(int(x) for x in tuple(r[0][2]))
+            
+            # Draw a rectangle around the text
+            img_rect = cv2.rectangle(img_rect, top_left, bottom_right, (0,255,0), 3)        
+            
+            # Fill text with white rectangle
+            img_temp = cv2.rectangle(img_temp, top_left, bottom_right, (255, 255, 255), -1)
+            
+            # Convert temp image to black and white for mask
+            mask = cv2.cvtColor(img_temp, cv2.COLOR_BGR2GRAY)
+            
+            # "Content-Fill" using mask (INPAINT_NS vs INPAINT_TELEA)
+            img_inpaint = cv2.inpaint(img_inpaint, mask, 3, cv2.INPAINT_TELEA)
+            
+            
+            if DEBUG:
+                print(r)
+            
+            
+            # Add text translation and location to a list
+            if TRANSLATE:
+                if not r[1].isnumeric():
+                    translation = GoogleTranslator(source=LANG, target='en').translate(r[1])
+                    trans_list.append((translation, top_left))
+                    
+                    if DEBUG:
+                        print(r[1] + "\t" + translation)
+                
+                else:
+                    trans_list.append((r[1], top_left))
+
+
+    # Add text from list to image
+    if TRANSLATE:
+        for t in trans_list:
+            cv2.putText(img_inpaint, t[0], t[1], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, 2)
+    
+    
+    # Show images (with rectangles and with text removed)
+    if DEBUG:
+        plt.figure(figsize=(7, 7))
+        plt.axis('off')
+        plt.imshow(cv2.cvtColor(img_rect, cv2.COLOR_BGR2RGB))
+        
+
+        # Final image
+        plt.figure(figsize=(7, 7))
+        plt.axis('off')
+        
+        
+        plt.imshow(cv2.cvtColor(img_inpaint, cv2.COLOR_BGR2RGB))
+        plt.show()
+    
+    
+    cv2.imwrite(IMAGE.replace(".png", "").replace(".jpg", "") + "_ocr.png", img_inpaint)
+    
+
+# Main
+if __name__ == "__main__":
+    OCR()
+    
